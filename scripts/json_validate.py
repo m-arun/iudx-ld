76666315d46e131e117ea5db9f7376942720ffa7
Rakshit Ramesh <rakshitadmar@gmail.com>
2019-06-12 17:10:00 +0530

    Minor

--- scripts/json_validate.py
+++ scripts/json_validate.py
@@ -1,15 +1,23 @@
-from jsonschema import validate
+import jsonschema
 import json
 
-schemaFile = "https://raw.githubusercontent.com/rraks/iudx-ld/master/base_schemas/iudx_resourceItem_schema.json"
+schemaFile = "base_schemas/iudx_resourceItem_schema.json"
 itemFile = "ex_items/testItem.json"
 
 
 with open(itemFile, "r") as f:
     item = json.load(f)
 
+with open(schemaFile, "r") as f:
+    schema = json.load(f)
+
 try:
-    validate(item, schemaFile)
-except Exception as e:
-    print(e)
+    jsonschema.validate(item, schema)
+    print("Valid item")
+except jsonschema.exceptions.ValidationError as errV:
+    print("Validation error")
+
+except jsonschema.exceptions.SchemaError as errS:
+ print("Schema Error Occured")
+ print(errS.message)
 
